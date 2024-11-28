f6233f55694d0698041ba3d3d7ae10986e94296d
nitishkmr005 <your.email@example.com>
2024-11-29 01:00:03 +0530

    index chanes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,19 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/personal-website/',
-  server: {
-    port: 3000,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   },
+  server: {
+    port: 3000
+  }
 })
